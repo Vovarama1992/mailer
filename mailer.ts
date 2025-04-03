b98468362dc29325f4361e6f7f93d76f0cb45282
Vovarama1992 <vovayhh9988@gmail.com>
2025-04-03 08:11:08 +0300

    'fix'

---
+++ mailer.ts
@@ -0,0 +1,57 @@
+import * as dotenv from 'dotenv'; 
+import nodemailer from 'nodemailer';
+import * as fs from 'fs';
+import * as path from 'path';
+
+
+dotenv.config();
+
+
+const templatePath = path.join(__dirname, 'template.html');
+let htmlTemplate = fs.readFileSync(templatePath, 'utf8');
+
+
+
+
+const ports: number[] = [25, 587, 465];
+
+
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: 465,  
+  secure: true, 
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+  logger: true,  
+  debug: true,   
+});
+
+
+const sendTestEmail = async (): Promise<void> => {
+  for (let i = 0; i < ports.length; i++) {
+    const port = ports[i];
+    try {
+      
+      console.log(`📝 Trying port: ${port}`);
+
+      
+      const emailContent = htmlTemplate.replace('{{name}}', 'David');
+
+      const info = await transporter.sendMail({
+        from: process.env.EMAIL_FROM, 
+        to: 'davidbadzgaradze@gmail.com',
+        subject: 'Test email from SMTP server',
+        html: emailContent,
+      });
+
+      console.log(`✅ Message sent on port ${port}: ${info.messageId}`);
+      break; 
+    } catch (err) {
+      console.error(`❌ Error sending email on port ${port}: ${err.message}`);
+    }
+  }
+};
+
+sendTestEmail();
